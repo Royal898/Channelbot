517d8032a35b98bbcce32369ab26a6cc5856f208
Royal898 <124570589+Royal898@users.noreply.github.com>
2023-06-02 08:09:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = "16341342"
+API_HASH     = "8d569ba1bdff190ed468f33d84fcb942"
+BOT_TOKEN    = "5829712048:AAE8nwPpIcnTuhphhIAIiap8VZaodTZz0gk"
+SESSION      = "AQARyoWIKFhWEuwLWKhm5YOWqvRdItZSGVAQ5HQlLrVSuYDiRcye1thbJHxK9VtzRAMdmvU3KiTg6TrJjrMM0r2mMhis1Kc7jZjZG87X7j82yey6AxneX1_vavsNh6uyZvVWVsWeWQzMvjpEcG56XLcSdUX2p8gLoq0QoiDzx7DmgCVVGvRxpr8Vv8B0xMjzXB7_gN6P8VxM4nuzetB6LWgL5uiU1g0kRGjQIT-3QemmLcTabbLln9oezMsYBvsjSMCSN3BC-ct9z6iT-gHtbLzxda8QKIyzU8GwcnRQsrVqWwIy54GKbBuxLiYw9w1aZjPtb0UrTS8N55-sb8ENI65uAAAAATXMBPQA"
+DATABASE_URI = "mongodb+srv://Arnav898:Arnav898@cluster0.t668vzw.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001978737721
+ADMIN        = 1851591526
+CHANNEL      = "@ModsTechz"
